db26858b1c2e91e8b53a7eae63f06bcde3c36c2f
Siarhei <siarhei.by@gmail.com>
2022-02-17 22:10:07 +0300

    feat: add working solution, 20 test passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n));
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+      result = `${str[i]}${result}`;
+  }
+
+  return +result;
 }
